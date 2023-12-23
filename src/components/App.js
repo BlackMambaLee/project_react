2053e4ea2a03eb8d2503ac19cbc9b414d6a22fa9
martinslee <grossomemartins@gmail.com>
2023-12-23 15:26:13 +0900

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-// import React, { useEffect, useState } from "react";
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "./basic/Footer";
@@ -6,17 +5,6 @@ import Footer from "./basic/Footer";
 import AppRouter from "./Router";
 
 function App() {
-  // const [api, setApi] = useState();
-  // const apiCall = async(event) => {
-  //     const res = await axios.get('/user/' + event)
-  //     const data = res.data;
-  //     setApi(data)
-  // };
-  
-  // useEffect(() => {
-  //     apiCall();
-  // });
-
   return (
     <>
       <AppRouter/>
