a7d51bd306bdde3304c8ec4251362c6a41beb627
martinslee <grossomemartins@gmail.com>
2024-01-03 20:22:33 +0900

    artifect test

---
+++ deploy/after_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 당신의 ECR 레지스트리 URI
+ECR_REGISTRY="850095925860.dkr.ecr.ap-northeast-2.amazonaws.com"
+# 당신의 ECR 저장소 이름
+ECR_REPOSITORY="react"
+# Docker 이미지 이름
+DOCKER_IMAGE_NAME="latest"
+# Docker 컨테이너 이름
+DOCKER_CONTAINER_NAME="REACT"
+
+# AWS CLI 설정
+AWS_CLI="/usr/bin/aws"
+AWS_REGION="ap-northeast-2"
+AWS_PROFILE="default"
+
+# Docker를 당신의 ECR 레지스트리에 인증
+# $($AWS_CLI ecr get-login --no-include-email --region $AWS_REGION --profile $AWS_PROFILE)
+$($AWS_CLI ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY)
+
+# ECR에서 최신 Docker 이미지를 가져옴
+docker pull $ECR_REGISTRY/$ECR_REPOSITORY:$DOCKER_IMAGE_NAME
+
+# 기존 컨테이너를 중지하고 삭제함 (만약 있을 경우)
+docker stop $DOCKER_CONTAINER_NAME || true
+docker rm $DOCKER_CONTAINER_NAME || true
+
+# Docker 컨테이너 실행
+docker run -d --name $DOCKER_CONTAINER_NAME -p 80:80 $ECR_REGISTRY/$ECR_REPOSITORY:$DOCKER_IMAGE_NAME
+
+# 추가적인 배포 작업은 여기에 추가 가능
